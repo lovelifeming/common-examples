648d4fcbd3a0ca70ba5b0b53d5313062961e1fb7
Shengming <453752303@qq.com>
2018-08-25 22:08:00 +0800

    Add thread join example code and CountDownLatch example code.

--- src/main/java/com/zsm/commonexample/thread/ConcurrentSCCDemo.java
+++ src/main/java/com/zsm/commonexample/thread/ConcurrentSCCDemo.java
@@ -1,6 +1,7 @@
 package com.zsm.commonexample.thread;
 
-import java.util.concurrent.Exchanger;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 
 
@@ -14,12 +15,14 @@ import java.util.concurrent.Semaphore;
  */
 public class ConcurrentSCCDemo
 {
+    //region Semaphore 信号量控制同步线程数
+
     /**
      * permits 初始化信号量，控制并发数。 fair 公平竞争资源 true为公平，false为不公平
      */
     private static Semaphore semaphore = new Semaphore(3, true);
 
-    public void semaphoretest()
+    public static void semaphoretest()
     {
         for (int i = 1; i <= 20; i++)
         {
@@ -55,5 +58,86 @@ public class ConcurrentSCCDemo
         semaphore.release();
         System.out.println(Thread.currentThread().getName() + " exit and release the lock!");
     }
+    //endregion
 
+    /**
+     * region CountDownLatch 和 CyclicBarrier 都能够实现线程之间的等待.
+     * CountDownLatch 是不能够重用的，而 CyclicBarrier 是可以重用的。
+     */
+
+    //region CountDownLatch 一般用于某个线程 A 等待若干个其他线程执行完任务之后，它才执行,类似join.
+    public static void countDownLatch()
+    {
+        int count = 2;
+        CountDownLatch latch = new CountDownLatch(count);
+        for (int i = 0; i < count; i++)
+        {
+            createThread(latch, "thread" + i);
+        }
+        try
+        {
+            System.out.println(Thread.currentThread().getName() + " entry execute code");
+            latch.await();
+            System.out.println(Thread.currentThread().getName() + " exit execute code");
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createThread(CountDownLatch latch, String threadName)
+    {
+        new Thread(() -> {
+            try
+            {
+                System.out.println(Thread.currentThread().getName() + " entry execute code");
+                Thread.sleep(new Random().nextInt(10));
+                System.out.println(Thread.currentThread().getName() + " exit execute code");
+                latch.countDown();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }, threadName).start();
+    }
+    //endregion
+
+    //region Thread join
+    public static void joinThread()
+        throws InterruptedException
+    {
+        Thread thread1 = new Thread(() -> {
+            try
+            {
+                System.out.println(Thread.currentThread().getName() + " entry execute code");
+                Thread.sleep(5);
+                System.out.println(Thread.currentThread().getName() + " exit execute code");
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }, "thread1");
+        Thread thread2 = new Thread(() -> {
+            try
+            {
+                System.out.println(Thread.currentThread().getName() + " entry execute code");
+                Thread.sleep(5);
+                System.out.println(Thread.currentThread().getName() + " exit execute code");
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }, "thread2");
+        thread1.start();
+        thread2.start();
+        System.out.println(Thread.currentThread().getName() + " entry execute code");
+        thread1.join();
+        thread2.join();
+        System.out.println(Thread.currentThread().getName() + " exit execute code");
+    }
+    //endregion
 }
